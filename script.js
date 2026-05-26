cf700229fd0c1157f99310dc687485518ef479e9
Ashish Murarka <murarkaashish6294@gmail.com>
2026-05-27 00:14:48 +0530

    Implemented Whatsapp-card popup open and close

--- script.js
+++ script.js
@@ -9,3 +9,24 @@ window.addEventListener("mousemove", (e) =>{
         cursor.style.transform = "translate(10%, 10%) scale(1)";
     }, 500);
 });
+
+let whatsappButton = document.querySelector('.whatsapp-button');
+let whatsappCard = document.querySelector('.whatsapp-card');
+let whatsappCloseButton = document.querySelector('.whatsapp-card-close-button');
+let flag = true;
+whatsappButton.addEventListener('click',()=>{
+    if(flag == true){
+        whatsappCard.style.display = "block";
+        flag = false;
+    }
+    else{
+        whatsappCard.style.display = "none";
+        flag = true;
+    }
+
+});
+
+whatsappCloseButton.addEventListener('click',()=>{
+    whatsappCard.style.display = "none";
+    flag = true;
+});
