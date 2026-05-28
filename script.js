072c286df22fe301e04b84145717db328f6af3d3
Ashish Murarka <murarkaashish6294@gmail.com>
2026-05-28 22:15:12 +0530

    added intro video for landing page and fixed hero section cursor position

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ let timeout;
 heroSection.addEventListener("mousemove", (e) =>{//wrong use define the listener in  a function then use with both header and hero
     cursor.style.left = e.clientX + "px";
     cursor.style.top = e.clientY + "px";
-    cursor.style.transform = "translate(10%, 10%) scale(0.95)";
+    cursor.style.transform = "translate(5%, 5%) scale(0.95)";
     clearTimeout(timeout);
     timeout = setTimeout(() =>{
-        cursor.style.transform = "translate(10%, 10%) scale(1)";
+        cursor.style.transform = "translate(5%, 5%) scale(1)";
     }, 500);
 });
 header.addEventListener("mousemove", (e) =>{//wrong use define the listener in  a function then use with both header and hero
@@ -41,3 +41,8 @@ whatsappCloseButton.addEventListener('click',()=>{
     whatsappCard.style.display = "none";
     flag = true;
 });
+
+let landingVideo = document.querySelector('.landing-video');
+    timeout = setTimeout(()=>{
+        landingVideo.style.display = "none";
+    }, 7500);
